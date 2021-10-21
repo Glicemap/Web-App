5a83981a7ca8c0193b79ccc76fbdf1fac4118bec
Gustavo Trivelatto <gu.trivelatto@usp.br>
2021-10-21 10:27:12 -0300

    fixing api error

--- src/api/useMock.js
+++ src/api/useMock.js
@@ -7,8 +7,8 @@ const utilizeMock = (axios) => {
   mock.onGet(/all-users/).reply(200, mocks);
   mock.onGet(/single-user/).reply(200, mocks.users[0]);
   mock.onGet(/get-new-code/).reply(200, mocks.newPatientCode[Math.floor(Math.random() * 10)]);
-  mock.onGet(/all-patients/).reply(200, mocks.patients);
-  mock.onGet(/all-notifications/).reply(200, mocks.notifications);
+  mock.onGet(/get-patients/).reply(200, mocks.patients);
+  mock.onGet(/get-notifications/).reply(200, mocks.notifications);
   mock.onPost(/read-notifications/).reply(200, []);
   mock.onDelete(/delete-notifications/).reply(200, []);
   mock.onGet(/get-settings/).reply(200, mocks.settings)
